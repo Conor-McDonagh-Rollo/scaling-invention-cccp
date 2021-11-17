8bc693ee7565fe0b146f18f64aaf25f861e8c5b9
conormcdr <conormcdr@gmail.com>
2021-11-17 09:35:02 +0000

    uwu

--- js/index.js
+++ js/index.js
@@ -2,3 +2,7 @@
 function rickroll() {
     window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
 }
+
+function frog() {
+    alert("FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG FROG ");
+}
